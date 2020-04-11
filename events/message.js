7bb0d20c35408d1fc857ee03c2d5d2dc180dbf1b
Pookiebot <61335384+Pookiebot@users.noreply.github.com>
2020-04-11 14:26:11 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,13 +1,75 @@
 /* eslint-disable */
-
+const DB = require("../modules/db.js");
+const mysql = require("mysql");
 module.exports = class {
   constructor(client) {
     this.client = client;
   }
 
   async run(message) {
+    const db = mysql.createConnection(DB);
+
     if (message.author.bot) return;
 
+    let generatedXp = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
+    db.connect(function(err) {
+      if (err) throw err;
+      db.query(
+        `SELECT * FROM players LEFT JOIN server ON id_server = server.server_name WHERE user_id = '${message.author.id}' AND server_id = '${message.guild.id}'`,
+        (err, rows) => {
+          if (err) throw err;
+
+          if (rows.length < 1) {
+            var sql = `INSERT INTO players (id_server, user_id, user, xp, level) VALUES ('${message.guild.name}', '${message.author.id}' , '${message.author.username}', '${generatedXp}', '1')`;
+            db.query(sql, function(err, result) {
+              if (err) throw err;
+              console.log("réussi");
+              message.channel
+                .send("tu a atteint le niveau 1")
+                .then(m => m.delete(2500));
+            });
+          } else {
+            let SNAME = rows[0].user_id;
+            let SID = rows[0].server_id;
+            if (SID !== message.guild.id) {
+              var sql1 = `INSERT INTO players (id_server, user_id, user, xp, level) VALUES ('${message.guild.name}', '${message.author.id}' , '${message.author.username}', '${generatedXp}', '1')`;
+              db.query(sql1, function(err, result) {
+                if (err) throw err;
+                console.log("réussi1");
+                message.channel
+                  .send("tu a atteint le niveau 1")
+                  .then(m => m.delete(2500));
+              });
+            } else if (SNAME === message.author.id) {
+              let xp = rows[0].xp;
+              let LEVEL = rows[0].level;
+              if (xp >= 300) {
+                var sql2 = `UPDATE players SET xp = '0', level ='${LEVEL +
+                  1}' WHERE user_id = '${message.author.id}' AND id_server= '${
+                  message.guild.name
+                }' `;
+                db.query(sql2, function(err, result) {
+                  if (err) throw err;
+                  console.log("réussi2");
+                  message.channel.send(
+                    `tu a monté un lv, tu es maintenant niveau ${LEVEL + 1}`
+                  );
+                });
+              } else {
+                var sql3 = `UPDATE players SET xp = '${xp +
+                  generatedXp}' WHERE user_id = '${
+                  message.author.id
+                }' AND id_server = '${message.guild.name}' `;
+                db.query(sql3, function(err, result) {
+                  if (err) throw err;
+                  console.log("réussi3");
+                });
+              }
+            }
+          }
+        }
+      );
+    });
     if (
       !message.channel.permissionsFor(message.guild.me).missing("SEND_MESSAGES")
     )
@@ -16,11 +78,24 @@ module.exports = class {
     // Paramètres
     const settings = this.client.getSettings(message.guild);
     message.settings = settings;
-
-    if (message.content.indexOf(settings.prefix) !== 0) return;
+    function pretes(guildID) {
+      return new Promise((resolve, reject) => {
+        db.query(
+          `SELECT prefix FROM server WHERE server_id = '${guildID}'`,
+          function(err, rows) {
+            if (err) return reject(err);
+            rows = rows[0].prefix;
+            resolve(rows);
+          }
+        );
+      });
+    }
+    const pref = await pretes(message.guild.id);
+    console.log(pref);
+    if (message.content.indexOf(pref) !== 0) return;
 
     const args = message.content
-      .slice(settings.prefix.length)
+      .slice(pref.length)
       .trim()
       .split(/ +/g);
     const command = args.shift().toLowerCase();
